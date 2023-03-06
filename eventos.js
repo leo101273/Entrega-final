3156c432228c7c1e3c30c3d3725c03e704396dd2
leo101273 <38758624+leo101273@users.noreply.github.com>
2023-03-06 16:10:29 -0300

    Add files via upload

---
+++ eventos.js
@@ -0,0 +1,8 @@
+function mostrarocultar(){
+    var x = document.getElementById("ocultar-y-mostrar");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
